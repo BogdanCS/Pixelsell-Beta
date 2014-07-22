d6ed9a4c90f43e0af48d5ba137c1e2e72b790bd6
BogdanCS <bogdan.stoian11@gmail.com>
2014-07-22 22:56:55 +0100

    implemented jquery

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,21 @@
-function showHeader ()
+function startDraw (evt)
 {
-  header = document.getElementById("cnt");
-  header.style.visibility = "visible";
+  var xPosition = evt.pageX;
+  var yPosition = evt.pageY;
+  alert ("Click position is " + xPosition + "," +  yPosition);
 }
+
+function selectMode()
+{
+  $("#cnt").removeClass("hidden");
+  $("#select").addClass("select-screen");
+
+  $("#select").on("click", startDraw);
+}
+
+
+$(document).ready(function (){
+
+  $("#cnt-button").on("click", selectMode);
+  
+});
