60ea2ff5ce4d2cf6f89100bb89ff4e7b120f2d9a
BogdanCS <bogdan.stoian11@gmail.com>
2014-07-23 22:47:19 +0100

    Added a plugin (animated) but it's not working and implemented a select tool (partially working

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,56 @@
 function startDraw (evt)
 {
-  var xPosition = evt.pageX;
-  var yPosition = evt.pageY;
-  alert ("Click position is " + xPosition + "," +  yPosition);
+  $(".blueRct").remove();
+  var x1Position = evt.pageX;
+  var y1Position = evt.pageY;
+  //alert ("Click position is " + x1Position + "," +  y1Position);
+  
+  function updateDraw (evt2)
+  {
+    var x2Position = evt2.pageX;
+    var y2Position = evt2.pageY;
+
+    var widthRct = Math.abs(x2Position - x1Position);
+    var heightRct = Math.abs(y2Position - y1Position);
+
+    // Not sure
+    var tmp = 0;
+    if (x2Position < x1Position)
+    {
+      tmp = x1Position;
+      x1Position = x2Position;
+      x2Position = tmp;
+    }
+
+    if (y2Position < y1Position)
+    {
+      tmp = y1Position;
+      y1Position = y2Position;
+      y2Position = tmp;
+    }
+    
+
+    var currentRct = $("<canvas/>", {'class' : 'blueRct'}).prop({width: widthRct,
+                                                                 height: heightRct});
+    currentRct.css({'top' : y1Position,
+		    'left': x1Position });
+
+    $(".blueRct").remove();
+    $("#select").append(currentRct);
+
+  }
+
+  $("#select").on("mousemove", updateDraw);
+
+  function finishDraw(evt3)
+  {
+    $("#select").off("mousemove", updateDraw);
+    // $(".blueRct").animatedBorder({size : 1, color : '#3b6cc'});
+    // do stuff
+  }
+
+  // finish draw on mouseup
+  $("#select").on("mouseup", finishDraw);
 }
 
 function selectMode()
@@ -10,12 +58,12 @@ function selectMode()
   $("#cnt").removeClass("hidden");
   $("#select").addClass("select-screen");
 
-  $("#select").on("click", startDraw);
+  $("#select").on("mousedown", startDraw);
 }
 
 
 $(document).ready(function (){
 
   $("#cnt-button").on("click", selectMode);
-  
+
 });
