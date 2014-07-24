641927958217b19127bbae36f98bf05be4c94553
BogdanCS <bogdan.stoian11@gmail.com>
2014-07-24 16:00:58 +0100

    first working select tool + price tag div

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,7 +4,9 @@ function startDraw (evt)
   var x1Position = evt.pageX;
   var y1Position = evt.pageY;
   //alert ("Click position is " + x1Position + "," +  y1Position);
-  
+  var price = 1;
+  var priceTag = $("<h2>1$</h2>");
+
   function updateDraw (evt2)
   {
     var x2Position = evt2.pageX;
@@ -13,31 +15,38 @@ function startDraw (evt)
     var widthRct = Math.abs(x2Position - x1Position);
     var heightRct = Math.abs(y2Position - y1Position);
 
-    // Not sure
-    var tmp = 0;
-    if (x2Position < x1Position)
-    {
-      tmp = x1Position;
-      x1Position = x2Position;
-      x2Position = tmp;
-    }
-
-    if (y2Position < y1Position)
-    {
-      tmp = y1Position;
-      y1Position = y2Position;
-      y2Position = tmp;
-    }
-    
-
     var currentRct = $("<canvas/>", {'class' : 'blueRct'}).prop({width: widthRct,
-                                                                 height: heightRct});
-    currentRct.css({'top' : y1Position,
-		    'left': x1Position });
+								 height: heightRct});
+
+    if (x2Position >= x1Position && y2Position >= y1Position)
+     currentRct.css({'top' : y1Position,
+		     'left': x1Position,
+		     'right': 'auto',
+		     'bottom': 'auto'});
+    else if (x2Position < x1Position && y2Position >= y1Position)
+      currentRct.css({'top' : y1Position,
+		      'left': 'auto',
+		      'right': $(window).width() - x1Position,
+		      'bottom': 'auto'});
+    else if (x2Position >= x1Position && y2Position < y1Position)
+      currentRct.css({'top' : 'auto',
+		      'left': x1Position,
+		      'right': 'auto',
+		      'bottom': $(window).height() - y1Position});
+    else
+       currentRct.css({'top' : 'auto',
+		       'left': 'auto',
+		       'right': $(window).width() - x1Position,
+		       'bottom': $(window).height() - y1Position});
 
     $(".blueRct").remove();
     $("#select").append(currentRct);
 
+    priceTag.text((widthRct * heightRct * price) +"$");
+    $("#priceCount").find("h2").remove();
+    $("#priceCount").append(priceTag);
+
+    // to add blocking stuff
   }
 
   $("#select").on("mousemove", updateDraw);
