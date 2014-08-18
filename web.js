48787b39dd5fc37531db46b06ee9b3b9917f149b
BogdanCS <bogdan.stoian11@gmail.com>
2014-08-18 23:21:54 +0300

    nice one, auto-writing to index.html finished

--- web.js
+++ web.js
@@ -48,53 +48,62 @@ app.listen(port, function()
 
 
 file = fs.createWriteStream(__dirname + '/public/index.html', {flags: "r+"});
-file.pos = 1325;
 var fullPaths = [];
 var greenLight = true;
+var filePosBackup = fs.createWriteStream(__dirname + '/backupVariables', {flags: "r+"});
+
+// Initialize file.pos
+file.pos = 1325;
+fs.readFile(__dirname + '/backupVariables', function (err, buffer){
+  file.pos = parseInt(buffer.toString());
+  console.log("file.pos after reading: " + file.pos);
+  });
 
 var timerId = setInterval( function(){
   if (greenLight)
-  { 
-  greenlight = false;
-  var path = __dirname + '/tmp/forIndex/';
-  fs.readdir(path, function(err, files){
-    async.mapLimit(files,1000,function(filename,cb){cb(null, path+filename);}, function(err, results){
-      fullPaths = results;
-      async.mapLimit(results,1000,fs.readFile, function(err, results){
-	// write results at pos in order
-	var writeCB = function(err, written, buffer)
+  {
+    greenlight = false;
+    var path = __dirname + '/tmp/forIndex/';
+    fs.readdir(path, function(err, files){
+      async.mapLimit(files,1000,function(filename,cb){cb(null, path+filename);}, function(err, results){
+	fullPaths = results;
+	async.mapLimit(results,1000,fs.readFile, function(err, results){
+	  // write results at pos in order
+	  var writeCB = function(err, written, buffer)
 	  {
-	      console.log("written");
-	      console.log(fullPaths[index]);
-	      console.log("file.pos before:" + file.pos);
-	      file.pos = file.pos - "</body></html>".length;
-	      console.log("file.pos before:" + file.pos);
-	      console.log(results[index].toString().length);
-	      fs.unlink(fullPaths[index], function (err) {if (err) throw err;
-							console.log("deleted");});
-	      console.log (results.length);
-	      if (index + 1 < results.length)
-	      {
-		  index++;
-		  file.write(new Buffer(results[index] + "</body></html>"), writeCB);
-	      }
-              else // we make sure that we don't have two instances(setInterval) running at the same time
-                greenLight = true;
+	    console.log("written");
+	    console.log(fullPaths[index]);
+	    console.log("file.pos before updating:" + file.pos);
+	    file.pos = file.pos - "</body></html>".length;
+            console.log ("filePosBackup: " + filePosBackup);
+	    filePosBackup.write(new Buffer(file.pos + ""), function (err, written, buffer){
+	      if (err) console.log ("backupVariables fail");
+	      else console.log("Written to bV: " + written + buffer);});
+	    console.log(results[index].toString().length);
+	    fs.unlink(fullPaths[index], function (err) {if (err) throw err;
+						      console.log("deleted");});
+	    console.log (results.length);
+	    if (index + 1 < results.length)
+	    {
+	      index++;
+	      file.write(new Buffer(results[index] + "</body></html>"), writeCB);
+	    }
+	    else // we make sure that we don't have two instances(setInterval) running at the same time
+	      greenLight = true;
 	  };
-	var index = 0;
-	console.log("penis");
-	console.log(results.length);
-	if (results.length > 0)
-	  {console.log("penis2");
-	   console.log(file.path);
-	   console.log(file.pos);
-	   console.log(results[index]);
-	   console.log(writeCB);
-	   file.write(new Buffer(results[index] + "</body></html>"),writeCB);}
-
-
+	  var index = 0;
+	  console.log("penis");
+	  console.log(results.length);
+	  if (results.length > 0)
+	  {
+	    console.log(file.path);
+	    console.log(file.pos);
+	    console.log(results[index]);
+	    console.log(writeCB);
+	    file.write(new Buffer(results[index] + "</body></html>"),writeCB);
+	  }
+      });
+     });
     });
-   });
-  });
   }
 },1000);
